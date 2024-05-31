f018b2c74c872897150c3eb981062ffde9fcdaab
xiao-qxl <1091843359@qq.com>
2024-05-31 14:41:44 +0800

    poker

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,53 @@
+import { log } from "console";
+
 // 定义牌的类型和常量
-type Color = "♥" | "♦" | "♣" | "♠";
-type Mark = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13;
+enum Color {
+    heart = "♥",
+    diamond = "♦",
+    club = "♣",
+    spade = "♠",
+}
+enum Mark {
+    A = "A",
+    two = "2",
+    three = "3",
+    four = "4",
+    five = "5",
+    six = "6",
+    seven = "7",
+    eight = "8",
+    nine = "9",
+    ten = "10",
+    jack = "J",
+    queen = "Q",
+    king = "K",
+}
 type NormalCard = {
     color: Color;
-    mark: number;
-};
-const NUM_COLORS = 4;
-const NUM_MARKS = 13;
-const MARK_MAP = {
-    1: "A",
-    11: "J",
-    12: "Q",
-    13: "K",
+    mark: string;
 };
 type Deck = NormalCard[];
 
-// 创建单张牌的函数
-function createCard(color: Color, mark: Mark): NormalCard {
-    return { color, mark };
-}
 // 创建一副牌
 function createDeck(): Deck {
     const deck: Deck = []
-    for (let i = 1; i <= 13; i++) {
-        deck.push({ color: '♠', mark: i })
-        deck.push({ color: '♣', mark: i })
-        deck.push({ color: '♥', mark: i })
-        deck.push({ color: '♦', mark: i })
+    const marks = Object.values(Mark)
+    const colors = Object.values(Color)
+    for (const m of marks) {
+        for (const c of colors) {
+            deck.push({ color: c, mark: m })
+        }
     }
-    return deck
+    return deck;
 }
 
 // 示例：打印一副牌，以验证功能
 function printDeck(deck: Deck) {
-    for (const card of deck) {
-        if (card.mark in MARK_MAP) {
-            console.log(`${card.color} ${MARK_MAP[card.mark]}`);
-        } else {
-            console.log(`${card.color} ${card.mark}`);
-        }
-    }
+    const text = deck.reduce((acc, card, i) => {
+        const tempAcc = acc + `${card.color} ${card.mark} \t`
+        return i % 4 === 3 ? tempAcc + "\n" : tempAcc
+    }, "")
+    console.log(text);
 }
 const deck = createDeck();
 printDeck(deck);
